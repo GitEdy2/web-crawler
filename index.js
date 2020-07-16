0906c78fcea2d0dc3b5467a9e95cdf6fc634cdde
Edison Proano <clanuzumaki22@gmail.com>
2020-07-15 21:50:58 -0500

    pulida de funciones.

--- index.js
+++ index.js
@@ -39,53 +39,51 @@ axios.get('https://news.ycombinator.com/')
             });
 
             return itemslist;
-
         }
 
-            const printHackerNews = () => {
-                return console.log(hackerNews());
-            }
+        const printHackerNews = () => {
+            return console.log(hackerNews());
+        }
+    
+        const filtercomments = () => {
+            let filter1 = itemslist.filter((item) => {
+                let tituloDividido = item.title.split(" ");
+                let numeroPalabras = tituloDividido.length;
+                
+                return numeroPalabras > 5;
+            })
+
+            let sortfilter1 = filter1.sort((a,b) => {
+                
+                    return parseInt(b.comments) - parseInt(a.comments);
+            }) 
+    
+            return console.log(sortfilter1);
+        }
         
-            const filtercomments = () => {
-                    let filter1 = itemslist.filter((item) => {
-                        let tituloDividido = item.title.split(" ");
-                        let numeroPalabras = tituloDividido.length;
-                        
-                        return numeroPalabras > 5;
-                })
         
-                    let sortfilter1 = filter1.sort((a,b) => {
-                        
-                            return parseInt(b.comments) - parseInt(a.comments);
-                }) 
-        
-                return console.log(sortfilter1);
-            }
-            
-            
-            const filterscores = () => {
-        
-                let filter2 = itemslist.filter((item) => {
-                    let tituloDividido = item.title.split(" ");
-                    let numeroPalabras = tituloDividido.length;
-        
-                    return numeroPalabras <= 5;
-                })
-        
-                let sortfilter2 = filter2.sort((a,b) => {
-        
-                        return parseInt(b.score) - parseInt(a.score);
-                })
-        
-                return console.log(sortfilter2);
-            }
+        const filterscores = () => {
+    
+            let filter2 = itemslist.filter((item) => {
+                let tituloDividido = item.title.split(" ");
+                let numeroPalabras = tituloDividido.length;
+    
+                return numeroPalabras <= 5;
+            })
+    
+            let sortfilter2 = filter2.sort((a,b) => {
+    
+                    return parseInt(b.score) - parseInt(a.score);
+            })
+    
+            return console.log(sortfilter2);
+        }
         
             printHackerNews();
             //filtercomments(hackerNews());
             //filterscores(hackerNews());
     })
     .catch(error => console.log(error));
-
 
     
     
