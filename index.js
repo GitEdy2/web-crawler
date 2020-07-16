0524c02f8e0af85ef139d197699c68d345c4c93c
Edison Proano <clanuzumaki22@gmail.com>
2020-07-15 20:41:41 -0500

    Creacion de funciones para web crawler.

---
+++ index.js
@@ -0,0 +1,114 @@
+const cheerio = require('cheerio');
+const axios = require('axios');
+
+axios.get('https://news.ycombinator.com/')
+    .then( res => {
+
+        let itemslist = [];
+
+        const hackerNews = () => {
+            const $ = cheerio.load(res.data);
+
+            $('.athing').each((index,el) => {
+                
+                let title = $(el)           
+                    .find('.title > .storylink')
+                    .first()
+                    .text();
+
+                let rank = $(el)
+                    .find('.athing > .title')
+                    .children('span')
+                    .first()
+                    .text();
+
+                let score = $(el)
+                    .next()
+                    .find('.subtext > .score')
+                    .first()
+                    .text()
+
+                let comments = $(el)
+                    .next()
+                    .find('.subtext')
+                    .children('a')
+                    .last()
+                    .text()
+
+                itemslist[index] = {rank,title,score,comments}
+            });
+
+            return itemslist;
+        }
+
+        const printHackerNews = () => {
+            return console.log(hackerNews());
+        }
+
+        const filtercomments = () => {
+                let filter1 = itemslist.filter((item) => {
+                    let tituloDividido = item.title.split(" ");
+                    let numeroPalabras = tituloDividido.length;
+                    
+                    return numeroPalabras > 5;
+            })
+
+                let sortfilter1 = filter1.sort((a,b) => {
+                    
+                        return parseInt(b.comments) - parseInt(a.comments);
+            }) 
+
+            return console.log(sortfilter1);
+        }
+        
+        
+        const filterscores = () => {
+
+            let filter2 = itemslist.filter((item) => {
+                let tituloDividido = item.title.split(" ");
+                let numeroPalabras = tituloDividido.length;
+
+                return numeroPalabras <= 5;
+            })
+
+            let sortfilter2 = filter2.sort((a,b) => {
+
+                    return parseInt(b.score) - parseInt(a.score);
+            })
+
+            return console.log(sortfilter2);
+        }
+        
+        //printHackerNews();
+        //filtercomments(hackerNews());
+        filterscores(hackerNews());
+    });        
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
