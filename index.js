b9118fbd0f18a4680ef75223fa21df3a4aca926c
Edison Proano <clanuzumaki22@gmail.com>
2020-07-15 21:14:00 -0500

    Promesas con funciones para web crawler.

--- index.js
+++ index.js
@@ -39,51 +39,58 @@ axios.get('https://news.ycombinator.com/')
             });
 
             return itemslist;
+
         }
 
-        const printHackerNews = () => {
-            return console.log(hackerNews());
-        }
-
-        const filtercomments = () => {
-                let filter1 = itemslist.filter((item) => {
+            const printHackerNews = () => {
+                return console.log(hackerNews());
+            }
+        
+            const filtercomments = () => {
+                    let filter1 = itemslist.filter((item) => {
+                        let tituloDividido = item.title.split(" ");
+                        let numeroPalabras = tituloDividido.length;
+                        
+                        return numeroPalabras > 5;
+                })
+        
+                    let sortfilter1 = filter1.sort((a,b) => {
+                        
+                            return parseInt(b.comments) - parseInt(a.comments);
+                }) 
+        
+                return console.log(sortfilter1);
+            }
+            
+            
+            const filterscores = () => {
+        
+                let filter2 = itemslist.filter((item) => {
                     let tituloDividido = item.title.split(" ");
                     let numeroPalabras = tituloDividido.length;
-                    
-                    return numeroPalabras > 5;
-            })
-
-                let sortfilter1 = filter1.sort((a,b) => {
-                    
-                        return parseInt(b.comments) - parseInt(a.comments);
-            }) 
-
-            return console.log(sortfilter1);
-        }
         
+                    return numeroPalabras <= 5;
+                })
         
-        const filterscores = () => {
-
-            let filter2 = itemslist.filter((item) => {
-                let tituloDividido = item.title.split(" ");
-                let numeroPalabras = tituloDividido.length;
-
-                return numeroPalabras <= 5;
-            })
-
-            let sortfilter2 = filter2.sort((a,b) => {
-
-                    return parseInt(b.score) - parseInt(a.score);
-            })
-
-            return console.log(sortfilter2);
-        }
+                let sortfilter2 = filter2.sort((a,b) => {
         
-        //printHackerNews();
-        //filtercomments(hackerNews());
-        //filterscores(hackerNews());
+                        return parseInt(b.score) - parseInt(a.score);
+                })
+        
+                return console.log(sortfilter2);
+            }
+        
+            printHackerNews();
+            //filtercomments(hackerNews());
+            //filterscores(hackerNews());
     })
-    .catch(error => console.log(error));     
+    .catch(error => console.log(error));
+
+
+    
+    
+    
+    
 
     
 
